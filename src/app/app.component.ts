cd94deb6f7f789d6a3c94dcebe1d413eb0bc00eb
qinhuajun <qinhuajun@outlook.com>
2024-12-19 23:20:42 +0800

    add home link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NzLayoutModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NzLayoutModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
